a3f09a81160517d13493b440aad3ddc68825ec1a
ariclenessousa <ariclenes.sousa87@gmail.com>
2022-03-28 03:37:49 -0300

    Finalizando o projeto da calculadora =D

--- calculo.js
+++ calculo.js
@@ -1,32 +1,31 @@
+
 const display = document.querySelector("#display");
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 
 buttons.forEach((item) => {
-    item.onclick = () => {
-     if (item.id == "clear") {
-         display.innerText = "";
-     }else if (item.amid == "backspace") {
-         let string = display.innerText.toString();
-        display.innerText = string.subtr(0, string.lenght -1);
-     }else if (display.innerText != '' && item.id == "igual") {
-         display.innerText = eval(display.innerText);
-     }else if (display.innerText == '' && item.id == "igual") {
-         display.innerText = "Empty!";
-         setTimeout(() => (display.innetText = ''), -2000);
-     }else {
-         display.innerText += item.id;
-     }
-             
-   }
-
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    } else if (item.id == "backspace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != "" && item.id == "igual") {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == "" && item.id == "igual") {
+      display.innerText = "Empty!";
+      setTimeout(() => (display.innerText = ""), 2000);
+    } else {
+      display.innerText += item.id;
+    }
+  };
 });
 
-const themeTogglebtn = document.querySelector(".theme-toggler");
-const calculadora = document.querySelector (".calculadora");
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculadora");
 const toggleIcon = document.querySelector(".toggler-icon");
 let isDark = true;
-themeTogglebtn.onclick = () => {
-    calculadora.classList.toggle("dark");
-    themeTogglebtn.classList.toggle("active");
-    isDark = !isDark;
-}
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+};
